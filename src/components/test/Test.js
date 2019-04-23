54be288e8bf45ff399eca2dab3ab548dd93be43f
Jared Monk <jmonk@jaredmonkdigital.com>
2019-04-23 15:04:03 -0500

    Detached from tutorial; reinitialized

---
+++ src/components/test/Test.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+export default class Test extends Component {
+  state = {
+    title: "",
+    body: ""
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          title: data.title,
+          body: data.body
+        })
+      );
+  }
+
+  // componentWillMount() {
+  //   console.log("ComponentWillMount");
+  // }
+
+  // componentDidUpdate() {
+  //   console.log("ComponentDidUpdate");
+  // }
+
+  // componentWillUpdate() {
+  //   console.log("ComponentWillUpdate");
+  // }
+
+  // componentWillReceiveProps(nextProps, nextState) {
+  //   console.log("componentWillReceiveProps");
+  // }
+
+  // static getDerivedStateFromProps(nextProps, prevState) {}
+
+  render() {
+    const { title, body } = this.state;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <p>{body}</p>
+      </div>
+    );
+  }
+}
